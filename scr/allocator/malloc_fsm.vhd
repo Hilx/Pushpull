c3808b4fce922a3d9d946e150cf7299e46e697ce
hilx <HILDA1897@GMAIL.COM>
2016-04-19 19:57:40 +0900

    updated, compiled

--- scr/allocator/malloc_fsm.vhd
+++ scr/allocator/malloc_fsm.vhd
@@ -1,104 +1,104 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.malloc_pack.all;               -- memory management package
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE work.malloc_pack.ALL;               -- memory management package
 
-entity mmu is  -- memory management unit a.k.a memory allocator
-  port(
-    clk      : in  std_logic;
-    rst      : in  std_logic;
-    arg      : in  allocator_com_type;
-    ret      : out allocator_com_type;
-    mcin     : in  mem_control_type;
-    mcout    : out mem_control_type;
-    mmu_init : in  mmu_init_type
+ENTITY mmu IS  -- memory management unit a.k.a memory allocator
+  PORT(
+    clk      : IN  STD_LOGIC;
+    rst      : IN  STD_LOGIC;
+    argu     : IN  allocator_com_type;
+    retu     : OUT allocator_com_type;
+    mcin     : IN  mem_control_type;
+    mcout    : OUT mem_control_type;
+    mmu_init : IN  mmu_init_type
     );
-end entity mmu;
+END ENTITY mmu;
 
-architecture syn_mmu of mmu is
+ARCHITECTURE syn_mmu OF mmu IS
 
-  signal hdList                : slv(31 downto 0);
-  signal mmu_state, mmu_nstate : allocator_state_type;
-begin
-  mmu_fsm_comb : process(mmu_state, arg, mcin, mmu_init)
-  begin
-    case mmu_state is
-      when mmu_state_idle =>
+  SIGNAL hdList                : slv(31 DOWNTO 0);
+  SIGNAL mmu_state, mmu_nstate : allocator_state_type;
+BEGIN
+  mmu_fsm_comb : PROCESS(mmu_state, argu, mcin, mmu_init)
+  BEGIN
+    CASE mmu_state IS
+      WHEN mmu_state_idle =>
         mmu_nstate <= mmu_state_idle;   -- default next state
-        if mmu_init.start = '1' then
+        IF mmu_init.start = '1' THEN
           mmu_nstate <= mmu_state_init;
-        end if;
-        if arg.start = '1' then         -- if there is a new request
+        END IF;
+        IF argu.start = '1' THEN        -- if there is a new request
           mmu_nstate <= mmu_state_malloc;    -- if malloc
-          if arg.cmd = free then        -- if free
+          IF argu.cmd = free THEN       -- if free
             mmu_nstate <= mmu_state_free;
-          end if;
-        end if;
-      when mmu_state_malloc =>
+          END IF;
+        END IF;
+      WHEN mmu_state_malloc =>
         mmu_nstate <= mmu_state_read_wait;
-      when mmu_state_read_wait =>
+      WHEN mmu_state_read_wait =>
         mmu_nstate <= mmu_state_read_wait;   -- stay in state by default
-        if mcin.done = '1' then         -- if read finished, go to done state
+        IF mcin.done = '1' THEN         -- if read finished, go to done state
           mmu_nstate <= mmu_state_done;
-        end if;
-      when mmu_state_free =>
+        END IF;
+      WHEN mmu_state_free =>
         mmu_nstate <= mmu_state_write_wait;
-      when mmu_state_write_wait =>
+      WHEN mmu_state_write_wait =>
         mmu_nstate <= mmu_state_write_wait;  -- stay in state by default
-        if mcin.done = '1' then         -- if write finished, go to done state
+        IF mcin.done = '1' THEN         -- if write finished, go to done state
           mmu_nstate <= mmu_state_done;
-        end if;
-      when mmu_state_done =>
+        END IF;
+      WHEN mmu_state_done =>
         mmu_nstate <= mmu_state_idle;
-      when mmu_state_init =>
+      WHEN mmu_state_init =>
         mmu_nstate <= mmu_state_init_wait;
-      when mmu_state_init_wait =>
+      WHEN mmu_state_init_wait =>
         mmu_nstate <= mmu_state_init_wait;
-        if mmu_init.done = '1' then
+        IF mmu_init.done = '1' THEN
           mmu_nstate <= mmu_state_idle;
-        end if;
-      when others =>
+        END IF;
+      WHEN OTHERS =>
         mmu_nstate <= mmu_state_idle;   -- to avoid latches
-    end case;
-  end process mmu_fsm_comb;
+    END CASE;
+  END PROCESS mmu_fsm_comb;
 
-  mmu_fsm_reg : process
-  begin
-    wait until clk'event and clk = '1';
+  mmu_fsm_reg : PROCESS
+  BEGIN
+    WAIT UNTIL clk'event AND clk = '1';
     -- update state
     mmu_state   <= mmu_nstate;
     -- default control signal values
     mcout.start <= '0';
-    ret.done    <= '0';
+    retu.done   <= '0';
 
-    if rst = CONST_RESET then
+    IF rst = CONST_RESET THEN
       mmu_state <= mmu_state_idle;
-    else
-      case mmu_state is
-        when mmu_state_malloc =>        -- in state malloc
-          ret.ptr     <= hdList;   -- return the hdList as allocated pointer
+    ELSE
+      CASE mmu_state IS
+        WHEN mmu_state_malloc =>        -- in state malloc
+          retu.ptr    <= hdList;    -- retuurn the hdList as allocated pointer
           -- to update list of mem blocks
           mcout.start <= '1';
           mcout.cmd   <= mc_read;
           mcout.addr  <= hdList;
-        when mmu_state_free =>          -- in state free
+        WHEN mmu_state_free =>          -- in state free
           -- to update list of mem blocks
           mcout.start <= '1';
           mcout.cmd   <= mc_write;
-          mcout.addr  <= arg.ptr;       -- write in header of freed mem block
+          mcout.addr  <= argu.ptr;      -- write in header of freed mem block
           mcout.data  <= hdList;        -- the old hdList
-        when mmu_state_done =>          -- in state done
-          ret.done <= '1';
+        WHEN mmu_state_done =>          -- in state done
+          retu.done <= '1';
           -- update hdList
-          hdList   <= mcin.data;         -- if malloc
-          if arg.cmd = free then
-            hdList <= arg.ptr;
-          end if;
-        when mmu_state_init =>          -- assign initial hdList address
+          hdList    <= mcin.data;       -- if malloc
+          IF argu.cmd = free THEN
+            hdList <= argu.ptr;
+          END IF;
+        WHEN mmu_state_init =>          -- assign initial hdList address
           hdList <= MEM_BASE;
-        when others => null;
-      end case;
-    end if;  -- reset or not
-  end process mmu_fsm_reg;
+        WHEN OTHERS => NULL;
+      END CASE;
+    END IF;  -- reset or not
+  END PROCESS mmu_fsm_reg;
 
-end architecture syn_mmu;
+END ARCHITECTURE syn_mmu;
