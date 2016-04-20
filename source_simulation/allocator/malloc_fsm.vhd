9f8cc0e5a763c2bf90c1a7f3ab9efb693cb89fb8
hilx <HILDA1897@GMAIL.COM>
2016-04-20 14:34:44 +0900

    structured

---
+++ source_simulation/allocator/malloc_fsm.vhd
@@ -0,0 +1,104 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE work.malloc_pack.ALL;               -- memory management package
+
+ENTITY mmu IS  -- memory management unit a.k.a memory allocator
+  PORT(
+    clk      : IN  STD_LOGIC;
+    rst      : IN  STD_LOGIC;
+    argu     : IN  allocator_com_type;
+    retu     : OUT allocator_com_type;
+    mcin     : IN  mem_control_type;
+    mcout    : OUT mem_control_type;
+    mmu_init : IN  mmu_init_type
+    );
+END ENTITY mmu;
+
+ARCHITECTURE syn_mmu OF mmu IS
+  SIGNAL hdList                : slv(31 DOWNTO 0);
+  SIGNAL mmu_state, mmu_nstate : allocator_state_type;
+BEGIN
+  
+  mmu_fsm_comb : PROCESS(mmu_state, argu, mcin, mmu_init)
+  BEGIN
+    CASE mmu_state IS
+      WHEN mmu_state_idle =>
+        mmu_nstate <= mmu_state_idle;   -- default next state
+        IF mmu_init.start = '1' THEN
+          mmu_nstate <= mmu_state_init;
+        END IF;
+        IF argu.start = '1' THEN        -- if there is a new request
+          mmu_nstate <= mmu_state_malloc;    -- if malloc
+          IF argu.cmd = free THEN       -- if free
+            mmu_nstate <= mmu_state_free;
+          END IF;
+        END IF;
+      WHEN mmu_state_malloc =>
+        mmu_nstate <= mmu_state_read_wait;
+      WHEN mmu_state_read_wait =>
+        mmu_nstate <= mmu_state_read_wait;   -- stay in state by default
+        IF mcin.done = '1' THEN         -- if read finished, go to done state
+          mmu_nstate <= mmu_state_done;
+        END IF;
+      WHEN mmu_state_free =>
+        mmu_nstate <= mmu_state_write_wait;
+      WHEN mmu_state_write_wait =>
+        mmu_nstate <= mmu_state_write_wait;  -- stay in state by default
+        IF mcin.done = '1' THEN         -- if write finished, go to done state
+          mmu_nstate <= mmu_state_done;
+        END IF;
+      WHEN mmu_state_done =>
+        mmu_nstate <= mmu_state_idle;
+      WHEN mmu_state_init =>
+        mmu_nstate <= mmu_state_init_wait;
+      WHEN mmu_state_init_wait =>
+        mmu_nstate <= mmu_state_init_wait;
+        IF mmu_init.done = '1' THEN
+          mmu_nstate <= mmu_state_idle;
+        END IF;
+      WHEN OTHERS =>
+        mmu_nstate <= mmu_state_idle;   -- to avoid latches
+    END CASE;
+  END PROCESS mmu_fsm_comb;
+
+  mmu_fsm_reg : PROCESS
+  BEGIN
+    WAIT UNTIL clk'event AND clk = '1';
+    -- update state
+    mmu_state   <= mmu_nstate;
+    -- default control signal values
+    mcout.start <= '0';
+    retu.done   <= '0';
+
+    IF rst = CONST_RESET THEN
+      mmu_state <= mmu_state_idle;
+    ELSE
+      CASE mmu_state IS
+        WHEN mmu_state_malloc =>        -- in state malloc
+          retu.ptr    <= hdList;    -- retuurn the hdList as allocated pointer
+          -- to update list of mem blocks
+          mcout.start <= '1';
+          mcout.cmd   <= mc_read;
+          mcout.addr  <= hdList;
+        WHEN mmu_state_free =>          -- in state free
+          -- to update list of mem blocks
+          mcout.start <= '1';
+          mcout.cmd   <= mc_write;
+          mcout.addr  <= argu.ptr;      -- write in header of freed mem block
+          mcout.data  <= hdList;        -- the old hdList
+        WHEN mmu_state_done =>          -- in state done
+          retu.done <= '1';
+          -- update hdList
+          hdList    <= mcin.data;       -- if malloc
+          IF argu.cmd = free THEN
+            hdList <= argu.ptr;
+          END IF;
+        WHEN mmu_state_init =>          -- assign initial hdList address
+          hdList <= MEM_BASE;
+        WHEN OTHERS => NULL;
+      END CASE;
+    END IF;  -- reset or not
+  END PROCESS mmu_fsm_reg;
+
+END ARCHITECTURE syn_mmu;
---
+++ source_simulation/allocator/malloc_wrapper.vhd
@@ -0,0 +1,96 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE work.ALL;
+USE work.malloc_pack.ALL;  -- memory management packageLIBRARY IEEE;
+
+ENTITY malloc_wrapper IS
+  PORT(
+    clk          : IN  STD_LOGIC;
+    rst          : IN  STD_LOGIC;
+    mmu_init_bit : IN  STD_LOGIC;
+    -- Interval/DS communication
+    argu         : IN  allocator_com_type;
+    retu         : OUT allocator_com_type;
+    -- External/Memory communication
+    memcon_in    : IN  mem_control_type;
+    memcon_out   : OUT mem_control_type
+    );
+END ENTITY;
+
+ARCHITECTURE syn_malloc_wrapper OF malloc_wrapper IS
+  -- wires/inter-connecting signals
+  SIGNAL mmu_init    : mmu_init_type;
+  SIGNAL mmu_mcin    : mem_control_type;
+  SIGNAL mmu_mcout   : mem_control_type;
+  SIGNAL init_input  : mem_control_type;
+  SIGNAL init_output : mem_control_type;
+
+  -- channel select FSM
+  TYPE malloc_wrapper_state IS (mw_state_norm, mw_state_init);
+  SIGNAL mw_state, mw_nstate : malloc_wrapper_state;
+BEGIN
+  -- wiring components
+  mmu_init.start <= mmu_init_bit;
+
+  mmu0 : ENTITY mmu
+    PORT MAP(
+      clk      => clk,
+      rst      => rst,
+      argu     => argu,
+      retu     => retu,
+      mcin     => mmu_mcin,
+      mcout    => mmu_mcout,
+      mmu_init => mmu_init
+      );
+
+  init0 : ENTITY mmu_init_block
+    PORT MAP(
+      clk   => clk,
+      rst   => rst,
+      start => mmu_init_bit,
+      done  => mmu_init.done,
+      mcin  => init_input,
+      mcout => init_output
+      );
+
+  -- channel select FSM
+  mw_sel_comb : PROCESS(mw_state, mmu_init_bit, mmu_init)
+  BEGIN
+    mw_nstate <= mw_state_norm;
+    CASE mw_state IS
+      WHEN mw_state_norm =>
+        -- fsm control
+        mw_nstate <= mw_state_norm;
+        IF mmu_init_bit = '1' THEN
+          mw_nstate <= mw_state_init;
+        END IF;
+      WHEN mw_state_init =>
+        mw_nstate <= mw_state_init;
+        IF mmu_init.done = '1' THEN
+          mw_nstate <= mw_state_norm;
+        END IF;
+    END CASE;
+
+  END PROCESS;
+
+  mw_sel_reg : PROCESS
+  BEGIN
+    WAIT UNTIL clk'event AND clk = '1';
+    mw_state <= mw_nstate;
+    IF rst = CONST_RESET THEN
+      mw_state <= mw_state_norm;
+    END IF;
+  END PROCESS;
+
+  mmu_mcin   <= memcon_in;
+  init_input <= memcon_in;
+  mw_sel_loggic : PROCESS(mw_state, mmu_mcout, init_output)
+  BEGIN
+    memcon_out <= mmu_mcout;
+    IF mw_state = mw_state_init THEN
+      memcon_out <= init_output;
+    END IF;
+  END PROCESS;
+  
+END ARCHITECTURE;
