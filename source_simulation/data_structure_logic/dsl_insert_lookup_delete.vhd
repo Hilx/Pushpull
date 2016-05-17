1c2cbbee0dfa3de9d05473527ff520fbe288531b
hilx <HILDA1897@GMAIL.COM>
2016-05-17 17:20:14 +0900

    correction

--- source_simulation/data_structure_logic/dsl_insert_lookup_delete.vhd
+++ source_simulation/data_structure_logic/dsl_insert_lookup_delete.vhd
@@ -224,11 +224,11 @@ BEGIN
           node_request.node.nextPtr <= nodeIn.ptr;
           -- update prev node nextPtr if isn't inserting
           -- to start of bucket
-          IF nodePrev.ptr ! = entryPtr THEN
+          IF nodePrev.ptr /= entryPtr THEN
             nodePrev.nextPtr <= alloc_in.ptr;
           END IF;
         WHEN ins_wnode_done =>
-          IF nodePrev.ptr ! = entryPtr THEN
+          IF nodePrev.ptr /= entryPtr THEN
             node_request.start <= '1';
             node_request.cmd   <= wnode;
             node_request.ptr   <= nodePrev.ptr;
